c900855423f82e4df9182663b9e1d4b584df3110
ch1999ahmad <ch1999ahmad@gmail.com>
2021-05-31 01:07:14 +0500

    push

--- src/models/UserModal.js
+++ src/models/UserModal.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     city: { type: String, required: true },
+    // city: { type: String, required: true },
     avatar: { type: String, default: "" },
     clientID: { type: String, default: "" },
 })
