86101a55aed89f8e9d165060f611818b39d76b40
ch1999ahmad <ch1999ahmad@gmail.com>
2021-06-02 23:19:00 +0500

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('./src/db/connection')
 let port = process.env.PORT || 3000
 app.listen(port, () => console.log('listenning:', port))
+
 const cors=require('cors')
 const User = require('./src/models/UserModal')
 const Item = require('./src/models/ItemModal')
@@ -14,7 +15,9 @@ app.use(express.static("public"));
 
 app.use(express.json({ limit: '1mb' }));
 
+
 app.use(cors())
+
 app.post("/register", (req, res) => {
     let user = new User(req.body);
     user.save().then(() => {
